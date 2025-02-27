f1022ce1b4086afcb58082475fe7c5ec4dee77c8
spandanaravula552 <143260906+spandanaravula552@users.noreply.github.com>
2025-02-27 11:22:26 +0530

    Updated type of variable

--- variables.tf
+++ variables.tf
@@ -1,22 +1,28 @@
 variable "cidr" {
+  description = 
+  type = string 
   default = "10.0.0.0/16"
 }
 
 variable "ami" {
   description = "AMI ID for EC2 instances"
+  type = string
   default     = "ami-0261755bbcb8c4a84"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
+  type = string
   default     = "t2.micro"
 }
 
 variable "subnet1_cidr" {
+  type = string
   default = "10.0.0.0/24"
 }
 
 variable "subnet2_cidr" {
+  type = string
   default = "10.0.1.0/24"
 }
 
@@ -26,5 +32,6 @@ variable "availability_zones" {
 }
 
 variable "bucket_name" {
+  type = string
   default = "yugendars3storage"
 }
